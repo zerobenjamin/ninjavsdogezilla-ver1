006fa3fb2cd58fc1afd7586bbc2dbb3d198bd732
zerobenjamin <mikeyimmy@gmail.com>
2025-03-29 21:15:06 +0800

    Add development configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,30 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(m4a|mp3|wav)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'sounds/'
-            }
-          }
-        ]
-      }
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.m4a', '.mp3', '.wav'],
-  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-    }),
-  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
-    host: 'localhost',
-    port: 9000,
-    open: true,
-    hot: true,
     compress: true,
-    historyApiFallback: true,
-    client: {
-      overlay: true,
-    },
+    port: 8080,
+    hot: true,
+    host: '0.0.0.0',
   },
 }; 
